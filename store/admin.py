891a76c7e7b7b3e72a860b147555ed32a69f29e7
MGX <admin@mgx.dev>
2025-05-13 07:55:40 -0700

    Added wishlist functionality.

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ShoeCategory, Product, ShoeSize, ProductImage
+from .models import ShoeCategory, Product, ShoeSize, ProductImage, Order, OrderItem
 
 @admin.register(ShoeCategory)
 class ShoeCategoryAdmin(admin.ModelAdmin):
@@ -26,3 +26,20 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(ProductImage)
 class ProductImageAdmin(admin.ModelAdmin):
     list_display = ['product', 'image', 'is_main', 'order']
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+    readonly_fields = ['product', 'price', 'quantity', 'size']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email', 'city', 'status', 'created_at', 'total_cost']
+    list_filter = ['status', 'created_at']
+    readonly_fields = ['id', 'created_at', 'updated_at', 'total_cost']
+    inlines = [OrderItemInline]
+    
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['order', 'product', 'price', 'quantity', 'size']
+    list_filter = ['order']
